cd4c4762f1a3923d2c47472261202f884e07e1eb
Hris <xrisko06@abv.bg>
2026-04-04 19:56:20 +0300

    Add task

---
+++ Shell/Pipes/03-a-6000.sh
@@ -0,0 +1,60 @@
+# Копирайте <РЕПО>/exercises/data/emp.data във вашата home директория.
+cp /srv/fmi-os/exercises/data/emp.data ~
+
+# Посредством awk, използвайки копирания файл за входнни данни, изведете:
+
+# общия брой редове
+awk 'END { print NR }' ~/emp.data
+
+# третия ред
+awk 'NR == 3' ~/emp.data
+
+# последното поле от всеки ред
+awk '{ print $NF }' ~/emp.data
+
+# последното поле на последния ред
+awk 'END { print $NF }' ~/emp.data
+
+# всеки ред, който има повече от 4 полета
+awk 'NF > 4' ~/emp.data
+
+# всеки ред, чието последно поле е по-голямо от 4
+awk '$NF > 4' ~/emp.data
+
+# общия брой полета във всички редове
+awk 'END { print NF * NR }' ~/emp.data
+awk '{ fields += NF } END { print fields }' ~/emp.data
+
+# броя редове, в които се среща низът Beth
+awk '/Beth/ { matches++ } END { print matches }' ~/emp.data
+
+# най-голямото трето поле и редът, който го съдържа
+awk 'NR == 1 || $3 > max { max = $3; row = $0 } END { printf "Max: %s\nRow: %s\n", max, row }' ~/emp.data
+
+# всеки ред, който има поне едно поле
+awk 'NF >= 1' ~/emp.data
+
+# всеки ред, който има повече от 17 знака
+awk 'length($0) > 17' ~/emp.data
+awk '/.{18,}$/' ~/emp.data
+
+# броя на полетата във всеки ред и самият ред
+awk '{ printf "Fields: %s, Row: %s\n", NF, $0 }' ~/emp.data
+
+# първите две полета от всеки ред, с разменени места
+awk '{ print $2, $1 }' ~/emp.data
+
+# всеки ред така, че първите две полета да са с разменени места
+awk '{ temp = $1; $1 = $2; $2 = temp; print }' ~/emp.data
+
+# всеки ред така, че на мястото на първото поле да има номер на реда
+awk '{ $1 = NR; print }' ~/emp.data
+
+# всеки ред без второто поле
+awk '{ for (i = 1; i <= NF; i++) if (i != 2) printf "%s ", $i; printf "\n" }' ~/emp.data
+
+# за всеки ред, сумата от второ и трето поле
+awk '{ printf "Sum: %.2f\n", $2 + $3 }' ~/emp.data
+
+# сумата на второ и трето поле от всеки ред
+awk 'BEGIN { sum = 0 } { sum += $2 + $ 3} END { printf "Sum: %.2f\n", sum }' ~/emp.data
